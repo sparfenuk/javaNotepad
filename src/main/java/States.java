1baab60d58c186be3ec16a9e9ea44012a46db2d0
pacman_n <sparfenuk13@gmail.com>
2019-05-08 21:02:29 +0300

    States

---
+++ src/main/java/States.java
@@ -0,0 +1,7 @@
+public final class States {
+    public static final String READY = "READY";
+    public static final String LOADING = "LOADING";
+    public static final String SAVING = "SAVING";
+    public static final String EDITING = "EDITING";
+
+}
