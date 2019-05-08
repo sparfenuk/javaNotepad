30728b86164f8c1dd507c0a5d7d902ab8c288e75
pacman_n <sparfenuk13@gmail.com>
2019-05-08 21:12:42 +0300

    States

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -7,7 +7,14 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import javax.swing.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainController {
+
+    private String state;
 
     @FXML
     private TextField path;
@@ -30,6 +37,37 @@ public class MainController {
     @FXML
     private ProgressBar progressBar;
 
+    public MainController() {
+        state = States.READY;
+    }
+
+    private void saveFile()
+    {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    progressBar.setProgress(0);
+                    FileWriter fileWriter = new FileWriter(path.getText(),false);
+
+                    fileWriter.write(rtBox.getText());
+
+                    fileWriter.close();
+                    progressBar.setProgress(100);
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null,"Path not valid");
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+
+    }
+
+
     @FXML
     void onFibonacciClick(ActionEvent event) {
 
@@ -38,11 +76,34 @@ public class MainController {
     @FXML
     void onLoadClick(ActionEvent event) {
 
+        Runnable runnable = new Runnable() {
+
+            public void run() {
+                try {
+                    FileReader fileReader = new FileReader( path.getText());
+
+                    int data = fileReader.read();
+                    while(data != -1) {
+                        rtBox.setText(rtBox.getText() + (char)data);
+
+                        data = fileReader.read();
+                    }
+                    fileReader.close();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
     @FXML
     void onSaveClick(ActionEvent event) {
-
+        saveFile();
     }
 
 }
