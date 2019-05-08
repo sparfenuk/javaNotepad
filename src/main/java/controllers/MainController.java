dd76a01369d21c271a1bfdb69c88866f9318666f
pacman_n <sparfenuk13@gmail.com>
2019-05-08 19:44:29 +0300

    first

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,23 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField path;
+
+    @FXML
+    private TextArea rtBox;
+
+    @FXML
+    private Button loadBtn;
+
+    @FXML
+    private Button saveBtn;
+
+}
+
+
