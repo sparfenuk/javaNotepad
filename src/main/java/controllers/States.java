67390e39c8a7eb685aea4a26febe952129249ae3
pacman_n <sparfenuk13@gmail.com>
2019-05-08 21:08:06 +0300

    States

--- src/main/java/controllers/States.java
+++ src/main/java/controllers/States.java
@@ -1,7 +1,10 @@
+package controllers;
+
 public final class States {
     public static final String READY = "READY";
     public static final String LOADING = "LOADING";
     public static final String SAVING = "SAVING";
     public static final String EDITING = "EDITING";
+    public static final String SEARCHINGX = "SEARCHINGX";
 
 }
